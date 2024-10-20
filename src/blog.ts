68545c8e9423c7de68a168091994fcc63d89d304
Andrew <arvizumunozandrew@gmail.com>
2024-10-20 15:54:31 -0700

    dynamic append

--- src/blog.ts
+++ src/blog.ts
@@ -1,5 +1,27 @@
 
 
+
+
+
+type Person = {
+    first: string;
+    last: string;
+    age: string;
+}
+
+const persons: Person[] = [
+    {
+        first: "radahn",
+        last: "starscourge",
+        age: "1200 years",
+    },
+    {
+        first: "morgott",
+        last: "veiled",
+        age: "1800 years",
+
+    }
+];
 export type Blog = {
     title: string;
     date: string;
@@ -10,3 +32,48 @@ export type Blog = {
 
 
 };
+const blogs: Blog[] = [
+    {
+        title: "Underwater ceramic technician at a multimillion dollar corporation",
+        date: "June 2026",
+        description: "aquired ",
+        image: "./hank3.png",
+        imageAlt: "./hank8/jpg",
+        slug:"https://www.indeed.com/q-mcdonald's-l-niles,-il-jobs.html?vjk=ca9f610de6765032",
+    }, 
+    {
+        title: "job",
+        date: "June 2026",
+        description: "aquired ",
+        image: "./hank3.png",
+        imageAlt: "n",
+        slug:"https://www.indeed.com/q-mcdonald's-l-niles,-il-jobs.html?vjk=ca9f610de6765032",
+
+    }
+
+]
+
+
+export function displayBlog(): void {
+    const blogcontainer = document.getElementById("blog-container");
+
+    blogs.forEach((blog) => {
+        const blogDiv = document.createElement('div');
+        blogDiv.classList.add('post');
+        const title = document.createElement('h1');
+        title.textContent = blog.title;
+        const description = document.createElement('p');
+        description.textContent = blog.description;
+        const image = document.createElement('img');
+        image.src =blog.image;
+        blogDiv.appendChild(title);
+        blogDiv.appendChild(description);
+        blogDiv.appendChild(image);
+
+
+        blogcontainer?.appendChild(blogDiv);
+
+
+    });
+
+}
